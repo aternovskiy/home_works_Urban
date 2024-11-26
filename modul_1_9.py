fb635b592b535c6fec1ba2a807fd1b217f4885b0
aternovskiy <inphorce69@gmail.com>
2024-11-26 23:00:27 +0300

    Add files via upload

---
+++ modul_1_9.py
@@ -0,0 +1,12 @@
+example = 'слово'
+print(example[0])
+print(example[-1])
+print(example[2:])
+print(example[::-1])
+print(example[1::2])
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
