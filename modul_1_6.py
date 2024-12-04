e15fb2d08a3b0845558e6e7cc16532da009b4e52
aternovskiy <inphorce69@gmail.com>
2024-12-04 22:18:47 +0300

    Update and rename modul_1_6.py to modul_1_2.py

--- modul_1_6.py
+++
@@ -1,6 +0,0 @@
-number_of_completed_homeworks = 12
-number_of_hours = 1.5
-course_name = 'Python'
-print(number_of_hours / number_of_completed_homeworks)
-time_of_one_task = 0.125
-print('Курс:',course_name,', всего задач:',number_of_completed_homeworks,', затрачено часов:', number_of_hours,', среднее время выполнения ',time_of_one_task,'часа.')
